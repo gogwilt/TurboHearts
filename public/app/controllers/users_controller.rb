7f6c560da80004cb03690d39d5803fc266d5a984
gogwilt <gogwilt@gmail.com>
2012-11-25 22:00:33 -0800

    Checkpointing work automatically

---
+++ public/app/controllers/users_controller.rb
@@ -0,0 +1,105 @@
+class UsersController < ApplicationController
+  # GET /users
+  # GET /users.json
+  def index
+    @users = User.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @users }
+    end
+  end
+
+  # GET /users/1
+  # GET /users/1.json
+  def show
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @user }
+    end
+  end
+
+  # GET /users/new
+  # GET /users/new.json
+  def new
+    @user = User.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @user }
+    end
+  end
+
+  # GET /users/1/edit
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  # POST /users
+  # POST /users.json
+  def create
+    @user = User.new(params[:user])
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render json: @user, status: :created, location: @user }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /users/1
+  # PUT /users/1.json
+  def update
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { head :ok }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /users/1
+  # DELETE /users/1.json
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    respond_to do |format|
+      format.html { redirect_to users_url }
+      format.json { head :ok }
+    end
+  end
+  
+  def do_login
+    @user = User.find(params[:name])
+    if @user
+      session[:user_id] = @user.id
+      redirect_to root_url, :notice => "Logged in successfully"
+    else
+      session[:user_id] = nil
+      redirect_to root_path, :notice => "Failed to log in"
+    end
+  end
+  
+  def do_logout
+    session[:user_id] = nil
+    redirect_to users_path, :notice => "Successfully logged out"
+  end
+  
+  def get_user
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+    end
+  end
+end
